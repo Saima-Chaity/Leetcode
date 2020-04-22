04b7c99eea46d8dc69d728c45fec4be7c2848e5d
Saima-Chaity <saima.s.chaity@gmail.com>
2020-04-22 14:01:21 -0700

    Solution for - Nested List Weight Sum I, II

---
+++ List/nestedListWeightSum.py
@@ -0,0 +1,146 @@
+# Nested List Weight Sum - https://leetcode.com/problems/nested-list-weight-sum/
+'''Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+
+Input: [1,[4,[6]]]
+Output: 27
+Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27.'''
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        return self.depthTotal(nestedList, 1)
+
+    def depthTotal(self, nestedList, depth):
+        totalSum = 0
+        for num in nestedList:
+            if num.isInteger():
+                totalSum += num.getInteger() * depth
+            else:
+                totalSum += self.depthTotal(num.getList(), depth + 1)
+        return totalSum
+
+
+# Nested List Weight Sum II - https://leetcode.com/problems/nested-list-weight-sum-ii/
+'''Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+The weight is defined from bottom up. i.e., the leaf level integers have weight 1, and the root level integers 
+have the largest weight.
+
+Example 1:
+Input: [1,[4,[6]]]
+Output: 17 
+Explanation: One 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17.'''
+
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        maxDepth = self.getMaxDepth(nestedList)
+        self.totalSum = 0
+        self.depthSum(nestedList, 1, maxDepth)
+        return self.totalSum
+
+    def getMaxDepth(self, nestedList):
+        currentMax = 1
+        for num in nestedList:
+            if num.isInteger() == False:
+                currentMax = max(currentMax, 1 + self.getMaxDepth(num.getList()))
+        return currentMax
+
+    def depthSum(self, nestedList, level, maxDepth):
+        for num in nestedList:
+            if num.isInteger():
+                self.totalSum = self.totalSum + num.getInteger() * (maxDepth - level + 1)
+            else:
+                self.depthSum(num.getList(), level + 1, maxDepth)
+        return
