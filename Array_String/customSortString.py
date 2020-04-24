aa52e7edbfdeac0935e4652309347dc683adc086
Saima-Chaity <saima.s.chaity@gmail.com>
2020-04-23 21:06:22 -0700

    Solution for - Custom Sort String

---
+++ Array_String/customSortString.py
@@ -0,0 +1,28 @@
+# Custom Sort String - https://leetcode.com/problems/custom-sort-string/
+'''S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they match the order
+that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+
+Return any permutation of T (as a string) that satisfies this property.
+
+Example :
+Input:
+S = "cba"
+T = "abcd"
+Output: "cbad"'''
+
+
+class Solution:
+    def customSortString(self, S: str, T: str) -> str:
+
+        output = []
+        count = collections.Counter(T)
+        for char in S:
+            output.append(char * count[char])
+            count[char] = 0
+
+        for char in count:
+            output.append(char * count[char])
+
+        return "".join(output)
