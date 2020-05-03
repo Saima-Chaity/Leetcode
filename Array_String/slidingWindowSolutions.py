01e88f8e47d019990116c9318056582b5615f276
Saima-Chaity <saima.s.chaity@gmail.com>
2020-05-02 20:50:47 -0700

    Solution for - Permutation in String

--- Array_String/slidingWindowSolutions.py
+++ Array_String/slidingWindowSolutions.py
@@ -303,3 +303,45 @@ class Solution:
           left += eachWordLength
 
     return result
+
+
+# Permutation in String - https://leetcode.com/problems/permutation-in-string/
+'''Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, 
+one of the first string's permutations is the substring of the second string.
+
+Example 1:
+
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").'''
+
+
+class Solution:
+  def checkInclusion(self, s1: str, s2: str) -> bool:
+
+    if len(s1) > len(s2):
+      return False
+
+    mappingS1 = dict()
+    mappingS2 = dict()
+    for char in s1:
+      mappingS1[char] = mappingS1.get(char, 0) + 1
+
+    for i in range(len(s1)):
+      mappingS2[s2[i]] = mappingS2.get(s2[i], 0) + 1
+
+    if mappingS1 == mappingS2:
+      return True
+
+    for i in range(len(s1), len(s2)):
+      start = i - len(s1)
+
+      if mappingS2[s2[start]] == 1:
+        del mappingS2[s2[start]]
+      else:
+        mappingS2[s2[start]] -= 1
+      mappingS2[s2[i]] = mappingS2.get(s2[i], 0) + 1
+      if mappingS1 == mappingS2:
+        return True
+    return False
+
