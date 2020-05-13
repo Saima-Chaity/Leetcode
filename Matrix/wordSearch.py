d6f387e177b4ca5eddd2b7ffe578fcaa077ecad9
Saima-Chaity <saima.s.chaity@gmail.com>
2020-05-13 11:26:13 -0700

    Solution for - Word Search I, II

--- Matrix/wordSearch.py
+++ Matrix/wordSearch.py
@@ -49,3 +49,57 @@ class Solution:
                     if result:
                         return True
         return False
+
+
+# Word Search II - https://leetcode.com/problems/word-search-ii/
+'''Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally 
+or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+Example:
+
+Input: 
+board = [
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+words = ["oath","pea","eat","rain"]
+
+Output: ["eat","oath"]'''
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = {}
+        for word in words:
+            t = trie
+            for char in word:
+                if char not in t:
+                    t[char] = {}
+                t = t[char]
+            t["#"] = "#"
+
+        row = len(board)
+        col = len(board[0])
+        self.output = set()
+
+        for i in range(row):
+            for j in range(col):
+                self.checkNeighbor(i, j, board, trie, "")
+        return self.output
+
+    def checkNeighbor(self, i, j, board, trie, path):
+        if "#" in trie:
+            self.output.add(path)
+        if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]) or board[i][j] not in trie:
+            return
+        temp = board[i][j]
+        board[i][j] = "@"
+        self.checkNeighbor(i + 1, j, board, trie[temp], path + temp)
+        self.checkNeighbor(i - 1, j, board, trie[temp], path + temp)
+        self.checkNeighbor(i, j + 1, board, trie[temp], path + temp)
+        self.checkNeighbor(i, j - 1, board, trie[temp], path + temp)
+        board[i][j] = temp
