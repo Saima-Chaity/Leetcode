af5e38a6b16fee39baf94ec8b1cde2fb3a0cf1cc
Saima-Chaity <saima.s.chaity@gmail.com>
2020-05-13 16:53:42 -0700

    Solution for - Added another solution

--- Tree/lowestCommonAncestorOfABinaryTree.py
+++ Tree/lowestCommonAncestorOfABinaryTree.py
@@ -12,13 +12,12 @@ Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
 Output: 3
 Explanation: The LCA of nodes 5 and 1 is 3.'''
 
-
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
@@ -41,3 +40,15 @@ class Solution:
         while q not in ancestors:
             q = parent[q]
         return q
+
+# Another solution
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
