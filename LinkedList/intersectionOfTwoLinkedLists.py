2eec54854d74e5ed8478543c8715ca05ba8ec2a3
Saima-Chaity <saima.s.chaity@gmail.com>
2020-05-25 13:11:27 -0700

    Solution for - Intersection of Two Linked Lists

---
+++ LinkedList/intersectionOfTwoLinkedLists.py
@@ -0,0 +1,25 @@
+# Intersection of Two Linked Lists - https://leetcode.com/problems/intersection-of-two-linked-lists/
+'''Write a program to find the node at which the intersection of two singly linked lists begins.
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+Output: Reference of the node with value = 8
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        if headA == None or headB == None:
+            return None
+
+        A_pointer = headA
+        B_pointer = headB
+
+        while A_pointer != B_pointer:
+            A_pointer = headB if A_pointer == None else A_pointer.next
+            B_pointer = headA if B_pointer == None else B_pointer.next
+        return A_pointer
