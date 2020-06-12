11fe7233371e0859d51f586b2f3db01e50bd99f1
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-12 16:31:58 -0700

    Solution for - Remove Duplicates from Sorted List

---
+++ LinkedList/removeDuplicatesFromSortedList.py
@@ -0,0 +1,69 @@
+# Remove Duplicates from Sorted List - https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+'''Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+
+        if not head:
+            return None
+
+        current = head.next
+        prev = head
+
+        while current:
+            if current.val == prev.val:
+                prev.next = current.next
+                current = current.next
+            else:
+                prev = prev.next
+                current = current.next
+        return head
+
+
+# Remove Duplicates from Sorted List II - https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+'''Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers 
+from the original list.
+
+Return the linked list sorted as well.
+
+Example 1:
+
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+
+        dummy = prev = ListNode(0)
+        dummy.next = head
+
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
+            else:
+                prev = prev.next
+                head = head.next
+        return dummy.next
