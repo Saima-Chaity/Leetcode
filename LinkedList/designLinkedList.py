c211bb4eaaefb45598efb0f7c9e69494a501baac
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-16 14:28:55 -0700

    Solution for - Design Linked List in Singly and Doubly linked list

--- LinkedList/designLinkedList.py
+++ LinkedList/designLinkedList.py
@@ -32,6 +32,8 @@ linkedList.get(1);            // returns 2
 linkedList.deleteAtIndex(1);  // now the linked list is 1->3
 linkedList.get(1);            // returns 3'''
 
+#Singly linked list
+
 class ListNode:
 
     def __init__(self, x):
@@ -100,6 +102,113 @@ class MyLinkedList:
         for i in range(index):
             current = current.next
         current.next = current.next.next
+
+# MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
+
+
+#Doubly linked list
+
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        self.prev = None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.size = 0
+        self.head = ListNode(0)
+        self.tail = ListNode(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if index >= self.size or index < 0:
+            return -1
+
+        if index + 1 < self.size - index:
+            current = self.head
+            for i in range(index + 1):
+                current = current.next
+        else:
+            current = self.tail
+            for i in range(self.size - index):
+                current = current.prev
+        return current.val
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+        self.addAtIndex(0, val)
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+        self.addAtIndex(self.size, val)
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+        if index > self.size:
+            return
+        if index < 0:
+            index = 0
+        if index < self.size - index:
+            predecessor = self.head
+            for i in range(index):
+                predecessor = predecessor.next
+            successor = predecessor.next
+        else:
+            successor = self.tail
+            for i in range(self.size - index):
+                successor = successor.prev
+            predecessor = successor.prev
+
+        self.size += 1
+        newNode = ListNode(val)
+        newNode.prev = predecessor
+        newNode.next = successor
+        predecessor.next = newNode
+        successor.prev = newNode
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+        if index >= self.size or index < 0:
+            return
+        if index < self.size - index:
+            predecessor = self.head
+            for i in range(index):
+                predecessor = predecessor.next
+            successor = predecessor.next.next
+        else:
+            successor = self.tail
+            for i in range(self.size - index - 1):
+                successor = successor.prev
+            predecessor = successor.prev.prev
+
+        self.size -= 1
+        predecessor.next = successor
+        successor.prev = predecessor
 
 # MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
