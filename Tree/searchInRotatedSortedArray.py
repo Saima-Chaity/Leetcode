6ff1995b286ec64b2f7031ab6010e53beabb9c7d
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-20 22:14:40 -0700

    Solution for - Search in Rotated Sorted Array I and II

---
+++ Tree/searchInRotatedSortedArray.py
@@ -0,0 +1,75 @@
+# Search in Rotated Sorted Array - https://leetcode.com/problems/search-in-rotated-sorted-array/
+'''Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+'''
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[low]:
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if target <= nums[high] and target > nums[mid]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+
+
+# Search in Rotated Sorted Array II - https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+'''Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true'''
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return True
+            while low < mid and nums[low] == nums[mid]:
+                low += 1
+            if nums[mid] >= nums[low]:
+                if target >= nums[low] and target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if target <= nums[high] and target > nums[mid]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return False
