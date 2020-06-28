b3d9daa00a44c8908b12579f081c33dd18356ca8
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-28 11:27:19 -0700

    Solution for - Maximum Binary Tree

---
+++ Tree/maximumBinaryTree.py
@@ -0,0 +1,44 @@
+# Maximum Binary Tree - https://leetcode.com/problems/maximum-binary-tree/
+'''Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
+
+The root is the maximum number in the array.
+The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+Construct the maximum tree by the given array and output the root node of this tree.
+
+Example 1:
+Input: [3,2,1,6,0,5]
+Output: return the tree root node representing the following tree:
+
+      6
+    /   \
+   3     5
+    \    /
+     2  0
+       \
+        1'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        def constructTree(nums):
+            if len(nums) == 0:
+                return None
+            while len(nums) > 0:
+                maxIndex = nums.index(max(nums))
+                root = TreeNode(nums[maxIndex])
+                nums.remove(nums[maxIndex])
+                root.left = constructTree(nums[:maxIndex])
+                root.right = constructTree(nums[maxIndex:])
+                return root
+        return constructTree(nums)
