35d5c2878a733a697e22c987825860ab9842d8fb
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-27 17:06:15 -0700

    Solution for - Serialize and Deserialize BST

---
+++ Tree/serializeAndDeserializeBST.py
@@ -0,0 +1,62 @@
+# Serialize and Deserialize BST - https://leetcode.com/problems/serialize-and-deserialize-bst/
+'''Serialization is the process of converting a data structure or object into a sequence of bits so
+that it can be stored in a file or memory buffer, or transmitted across a network connection link to
+be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how
+your serialization/deserialization algorithm should work. You just need to ensure that a binary search
+tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.'''
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        """
+        #Preorder Traversal
+        if not root:
+            return ''
+
+        result = ""
+        stack = []
+        while root or stack:
+            while root:
+                result += str(root.val) + ","
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            root = root.right
+        return result
+
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        """
+        if not data:
+            return None
+        splitedData = data.split(",")
+        root = None
+        for value in splitedData:
+            if len(splitedData) > 0 and value != '':
+                root = self.deserializeTree(root, int(value))
+        return root
+
+    def deserializeTree(self, root, value):
+        if not root:
+            return TreeNode(value)
+        if value < root.val:
+            root.left = self.deserializeTree(root.left, value)
+        else:
+            root.right = self.deserializeTree(root.right, value)
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
