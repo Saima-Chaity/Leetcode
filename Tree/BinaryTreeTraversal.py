c4d411bb6a1f9af281f369e8391cbadf012b099d
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-28 18:56:25 -0700

    Solution for - Binary Tree Level Order Traversal II

--- Tree/BinaryTreeTraversal.py
+++ Tree/BinaryTreeTraversal.py
@@ -166,3 +166,48 @@ class Solution:
                     queue.append(node.right)
             level += 1
         return output
+
+# Binary Tree Level Order Traversal II - https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+'''Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]'''
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+
+        output = []
+        level = 0
+        queue = deque([root])
+        while queue:
+            output.append([])
+            for i in range(len(queue)):
+                node = queue.popleft()
+                output[level].append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level += 1
+        return output[::-1]
