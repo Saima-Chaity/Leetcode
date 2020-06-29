6991c9a88b9798fcb949b9250e936c287d4f13a3
Saima-Chaity <saima.s.chaity@gmail.com>
2020-06-28 19:37:24 -0700

    Removed wrong implementation

--- Tree/lowestCommonAncestorOfABinaryTree.py
+++ Tree/lowestCommonAncestorOfABinaryTree.py
@@ -40,15 +40,3 @@ class Solution:
         while q not in ancestors:
             q = parent[q]
         return q
-
-# Another solution
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-
-        while root:
-            if root.val > p.val and root.val > q.val:
-                root = root.left
-            elif root.val < p.val and root.val < q.val:
-                root = root.right
-            else:
-                return root
