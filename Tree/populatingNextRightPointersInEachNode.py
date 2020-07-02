445beada7de6e28055a1c732522ee39a4a32ce1d
Saima Chaity <saima.s.chaity@gmail.com>
2020-07-01 17:31:28 -0700

    Added solution for - Populating Next Right Pointers in Each Node II

--- Tree/populatingNextRightPointersInEachNode.py
+++ Tree/populatingNextRightPointersInEachNode.py
@@ -68,3 +68,49 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
         return root
+
+
+# Populating Next Right Pointers in Each Node II - https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+'''Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]'''
+
+from collections import deque
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+
+        queue = deque([root])
+        nextLevel = deque([])
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                nextLevel.append(node.left)
+            if node.right:
+                nextLevel.append(node.right)
+            if queue:
+                node.next = queue[0]
+            if not queue and nextLevel:
+                queue, nextLevel = nextLevel, queue
+        return root
+    
