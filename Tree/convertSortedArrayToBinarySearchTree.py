3df3eba4928e179b210f85e646a9ae076ebb1538
Saima-Chaity <saima.s.chaity@gmail.com>
2020-07-14 21:24:43 -0700

    Solution for - Convert Sorted Array to Binary Search Tree

--- Tree/convertSortedArrayToBinarySearchTree.py
+++ Tree/convertSortedArrayToBinarySearchTree.py
@@ -33,15 +33,54 @@ class Solution:
         def convertToBST(left, right):
             if left > right:
                 return None
-
             mid = (left + right) // 2
             node = TreeNode(nums[mid])
-
             if left == right:
                 return node
-
             node.left = convertToBST(left, mid - 1)
             node.right = convertToBST(mid + 1, right)
             return node
-
         return convertToBST(0, len(nums) - 1)
+
+
+# Iterative
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:
+            return None
+
+        left = 0
+        right = len(nums) - 1
+        root = TreeNode(0)
+        stack = []
+        stack.append(root)
+        stack.append(left)
+        stack.append(right)
+
+        while stack:
+            right = int(stack.pop())
+            left = int(stack.pop())
+            node = stack.pop()
+            mid = left + ((right - left) // 2)
+            node.val = nums[mid]
+
+            if left <= mid - 1:
+                node.left = TreeNode(0)
+                stack.append(node.left)
+                stack.append(left)
+                stack.append(mid - 1)
+
+            if right >= mid + 1:
+                node.right = TreeNode(0)
+                stack.append(node.right)
+                stack.append(mid + 1)
+                stack.append(right)
+        return root
+
+
