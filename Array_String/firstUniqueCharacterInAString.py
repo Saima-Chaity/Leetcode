1ef994cc094f985a7a346099671e9e14b6d1a5d6
Saima-Chaity <saima.s.chaity@gmail.com>
2020-07-14 22:17:02 -0700

    Solution for - First Unique Character in a String

---
+++ Array_String/firstUniqueCharacterInAString.py
@@ -0,0 +1,23 @@
+# First Unique Character in a String - https://leetcode.com/problems/first-unique-character-in-a-string/
+'''Given a string, find the first non-repeating character in it and return its index.
+If it doesn't exist, return -1.
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.'''
+
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+
+        if not s:
+            return -1
+
+        count = Counter(s)
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx
+        return -1
+
