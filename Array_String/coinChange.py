c0d8d79681e31ad49264b73951e43fbe84687aa5
Saima-Chaity <saima.s.chaity@gmail.com>
2020-07-19 16:17:24 -0700

    Solution for - Coin Change

--- Array_String/coinChange.py
+++ Array_String/coinChange.py
@@ -32,3 +32,15 @@ class Solution:
                     visited[nextValue] = True
                     q.append((totalCoins, nextValue))
         return -1
+
+# Dynamic programming
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)  # 1 because we are comparing with one coin every time
+        return dp[-1] if dp[-1] != float('inf') else - 1
