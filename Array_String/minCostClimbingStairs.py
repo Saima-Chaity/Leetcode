0a141b01a3df448f44f06f793ddeb94402a80b47
Saima-Chaity <saima.s.chaity@gmail.com>
2020-07-19 16:30:20 -0700

    Solution for - Min Cost Climbing Stairs

---
+++ Array_String/minCostClimbingStairs.py
@@ -0,0 +1,19 @@
+# Min Cost Climbing Stairs - https://leetcode.com/problems/min-cost-climbing-stairs/
+'''On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost
+to reach the top of the floor, and you can either start from the step with index 0, or the
+step with index 1.
+
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.'''
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [1] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i - 2] + cost[i], dp[i - 1] + cost[i])
+        return min(dp[-1], dp[-2])
