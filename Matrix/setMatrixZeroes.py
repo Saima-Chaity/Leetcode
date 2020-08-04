fcfceede55c2efaa31cf946413db1215a2cfe7f6
Saima-Chaity <saima.s.chaity@gmail.com>
2020-08-03 21:19:34 -0700

    Solution for - Set Matrix Zeroes

---
+++ Matrix/setMatrixZeroes.py
@@ -0,0 +1,73 @@
+# Set Matrix Zeroes - https://leetcode.com/problems/set-matrix-zeroes/
+'''Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+Input:
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+Output:
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]'''
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+        rowSet = set()
+        colSet = set()
+
+        for i in range(row):
+            for j in range(col):
+                if matrix[i][j] == 0:
+                    rowSet.add(i)
+                    colSet.add(j)
+
+        for i in range(row):
+            for j in range(col):
+                if i in rowSet or j in colSet:
+                    matrix[i][j] = 0
+
+# Without extra space
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+        is_Col = False
+
+        for i in range(row):
+            if matrix[i][0] == 0:
+                is_Col = True
+            for j in range(1, col):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        # Update the elements using the first row and first col
+        for i in range(1, row):
+            for j in range(1, col):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+
+        # Update first row
+        if matrix[0][0] == 0:
+            for j in range(col):
+                matrix[0][j] = 0
+
+        # Update first col
+        if is_Col:
+            for i in range(row):
+                matrix[i][0] = 0
+
+
+
+
