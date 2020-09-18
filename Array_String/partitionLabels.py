e26ccd7820cb99d529229bd5f11715684197e93f
Saima Chaity <saima.s.chaity@gmail.com>
2020-09-18 10:28:31 -0700

    Solution for - Partition Labels

---
+++ Array_String/partitionLabels.py
@@ -0,0 +1,32 @@
+# Partition Labels - https://leetcode.com/problems/partition-labels/
+'''A string S of lowercase English letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, 
+and return a list of integers representing the size of these parts.
+Example 1:
+Input: S = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+ 
+Note:
+S will have length in range [1, 500].
+S will consist of lowercase English letters ('a' to 'z') only.'''
+
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        mapping = {}
+        for index, char in enumerate(S):
+            mapping[char] = index
+
+        endIndex = 0
+        startIndex = 0
+        output = []
+        for index, char in enumerate(S):
+            endIndex = max(endIndex, mapping[char])
+            if index == endIndex:
+                output.append(index-startIndex+1)
+                startIndex = index + 1
+        return output
+            
+                    
