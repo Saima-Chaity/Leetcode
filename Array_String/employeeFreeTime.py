7748394f672d5798231e10589d522b30aa04283c
Saima Chaity <saima.s.chaity@gmail.com>
2020-09-19 21:34:45 -0700

    Solution for - Employee Free Time

---
+++ Array_String/employeeFreeTime.py
@@ -0,0 +1,44 @@
+# Employee Free Time - https://leetcode.com/problems/employee-free-time/
+'''We are given a list schedule of employees, which represents the working time for each employee.
+Each employee has a list of non-overlapping Intervals, and these intervals are in sorted order.
+Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
+(Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays. 
+For example, schedule[0][0].start = 1, schedule[0][0].end = 2, and schedule[0][0][0] is not defined).  
+Also, we wouldn't include intervals like [5, 5] in our answer, as they have zero length.
+
+Example 1:
+
+Input: schedule = [[[1,2],[5,6]],[[1,3]],[[4,10]]]
+Output: [[3,4]]
+Explanation: There are a total of three employees, and all common
+free time intervals would be [-inf, 1], [3, 4], [10, inf].
+We discard any intervals that contain inf as they aren't finite.'''
+
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+import heapq
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        
+        heap = []
+        for interval in schedule:
+            for item in interval:
+                heapq.heappush(heap, (item.start, item.end))
+
+        prev = None
+        output = []
+        while len(heap) > 0:
+            time = heapq.heappop(heap)
+            if prev:
+                if prev < time[0]:
+                    output.append(Interval(prev, time[0]))
+                prev = max(prev, time[1])
+            else:
+                prev = time[1]
+        return output
