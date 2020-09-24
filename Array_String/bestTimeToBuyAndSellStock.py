8e49a8eaa83583e3abbf1796a0f4072bb45615af
Saima Chaity <saima.s.chaity@gmail.com>
2020-09-23 19:33:04 -0700

    Modified solution

--- Array_String/bestTimeToBuyAndSellStock.py
+++ Array_String/bestTimeToBuyAndSellStock.py
@@ -36,12 +36,13 @@ Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
-        for i in range(1, len(prices)):
-            buy = prices[i-1]
-            sell = prices[i]
+        
+        for i in range(len(prices)-1):
+            buy = prices[i]
+            sell = prices[i+1]
             if buy < sell:
-                maxProfit += sell - buy
-        return maxProfit if maxProfit != float('-inf') else 0
+                maxProfit += sell-buy
+        return maxProfit 
 
 # Another approach
 class Solution:
@@ -63,3 +64,30 @@ class Solution:
             sell = prices[i]
             max_profit += sell - buy
         return max_profit
+
+
+
+# Best Time to Buy and Sell Stock III - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+'''Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+Example 1:
+
+Input: prices = [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        oneBuy = float('inf')
+        oneBuyOneSell = float('-inf')
+        twoBuy = float('inf')
+        twoBuyTwoSell = float('-inf')
+        for price in prices:
+            oneBuy = min(oneBuy, price) # Find the lowest buy
+            oneBuyOneSell = max(oneBuyOneSell, price - oneBuy) # Find the maximum profit 
+            twoBuy = min(twoBuy, price-oneBuyOneSell) # Mimimize the buy price again
+            twoBuyTwoSell = max(twoBuyTwoSell, price-twoBuy) # Find the maximum profit 
+        return twoBuyTwoSell
