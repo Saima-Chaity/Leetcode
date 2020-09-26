6d5f3f4b033817200c2795bf5e5920c0d541f16b
Saima Chaity <saima.s.chaity@gmail.com>
2020-09-26 12:40:07 -0700

    Moved file and added solution for - Palindrome Partitioning II

--- BackTrack/palindromePartitioning.py
+++ BackTrack/palindromePartitioning.py
@@ -24,3 +24,43 @@ class Solution:
                 
         backTrack(0, len(s), [], s)
         return output
+
+
+# Palindrome Partitioning II - https://leetcode.com/problems/palindrome-partitioning-ii/
+'''Given a string s, partition s such that every substring of the partition is a palindrome.
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+Example 1:
+
+Input: s = "aab"
+Output: 1
+Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cut.'''
+
+class Solution:
+    def minCut(self, s: str) -> int:
+
+        def isPalindrome(s, startIndex, endIndex):
+            temp = s[startIndex:endIndex+1]
+            return temp == temp[::-1]
+    
+        mapping = {}
+        def backTrack(start, s):
+            if start >= len(s) or isPalindrome(s, start, len(s)-1) :
+                return 0
+            
+            if start in mapping:
+                return mapping[start]
+                
+            result = float('inf')      
+            for i in range(start+1, len(s)+1):
+                if i != len(s) and isPalindrome(s, start, i-1):
+                    result = min(result, 1 + backTrack(i, s))
+            result = result if result != float('inf') else 0
+            mapping[start] = result
+            return mapping[start]
+
+    
+        if isPalindrome(s, 0, len(s)-1):
+            return 0
+        else:
+            return backTrack(0, s)
---
+++ BackTrack/palindromePermutation.py
@@ -0,0 +1,68 @@
+# Palindrome Permutation - https://leetcode.com/problems/palindrome-permutation/
+'''Given a string, determine if a permutation of the string could form a palindrome.
+Example 1:
+
+Input: "code"
+Output: false
+Example 2:
+
+Input: "aab"
+Output: true'''
+
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        output = []
+        for char in s:
+            if char not in output:
+                output.append(char)
+            else:
+                output.remove(char)
+        return len(output) == 0 or len(output) == 1
+
+
+
+# Palindrome Permutation II - https://leetcode.com/problems/palindrome-permutation-ii/
+'''Given a string s, return all the palindromic permutations (without duplicates) of it. 
+Return an empty list if no palindromic permutation could be form.
+
+Example 1:
+
+Input: "aabb"
+Output: ["abba", "baab"]
+Example 2:
+
+Input: "abc"
+Output: []'''
+
+from collections import Counter
+class Solution:
+    def generatePalindromes(self, s: str) -> List[str]:
+        
+        s_count = Counter(s)
+        # If the number of characters with odd number of occurences exceeds 1, 
+        # it indicates that no palindromic permutation is possible for s
+        mid = [char for char, freq in s_count.items() if freq%2]
+        if len(mid) > 1:
+            return []
+        mid = "" if mid == [] else mid[0]
+        half_s = "".join([char * (freq//2) for char, freq in s_count.items()]) # Take half of the string
+        
+        output = []
+        def backTrack(path, visited):
+            if len(path) == len(half_s):
+                path = path + mid + path[::-1]
+                output.append(path)
+                return
+            for i in range(0, len(half_s)):
+                if not visited[i]:
+                    if i > 0 and not visited[i-1] and half_s[i] == half_s[i-1]:
+                        continue
+                    visited[i] = True
+                    backTrack(path+half_s[i], visited)
+                    visited[i] = False
+
+        visited = [False] * len(s)
+        backTrack("", visited)
+        return output
+
+    
