982be99de3f31693a777aeeb55d2f133706fc882
Saima Chaity <saima.s.chaity@gmail.com>
2020-10-19 20:19:30 -0700

    Solution for - Palindromic Substrings

--- Array_String/longestPalindromicSubstring.py
+++ Array_String/longestPalindromicSubstring.py
@@ -31,3 +31,37 @@ class Solution:
         return s[left + 1:right]
 
 
+
+# Palindromic Substrings - https://leetcode.com/problems/palindromic-substrings/
+'''Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings 
+even they consist of same characters.
+
+Example 1:
+
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+ 
+Example 2:
+
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".'''
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        self.output = []
+        for i in range(len(s)):
+            p1 = self.getSubstr(i, i + 1, s)
+            p2 = self.getSubstr(i, i, s)
+
+        return len(self.output)
+
+    def getSubstr(self, left, right, s):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            self.output.append(s[left:right+1])
+            left -= 1
+            right += 1
+
