05afca7f27862276175aa22e810fbe477e5a7a52
Saima Chaity <saima.s.chaity@gmail.com>
2020-11-01 11:56:36 -0800

    Solution for - First Bad Version and Guess Number Higher or Lower

---
+++ Binary_Search/GuessNumberHigherOrLower.py
@@ -0,0 +1,42 @@
+# Guess Number Higher or Lower - https://leetcode.com/problems/guess-number-higher-or-lower/
+'''We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns 3 possible results:
+
+-1: The number I picked is lower than your guess (i.e. pick < num).
+1: The number I picked is higher than your guess (i.e. pick > num).
+0: The number I picked is equal to your guess (i.e. pick == num).
+Return the number that I picked.
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1'''
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        high = n
+        while low < high:
+            mid = low + (high-low) // 2
+            pickedNumber = guess(mid)
+            if pickedNumber == 0:
+                return mid
+            elif pickedNumber == 1:
+                low = mid + 1
+            else:
+                high = mid
+        return low
