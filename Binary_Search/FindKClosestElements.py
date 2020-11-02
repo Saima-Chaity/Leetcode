3651fbcdf020afe53bc6149804b2157ab519ff5b
Saima Chaity <saima.s.chaity@gmail.com>
2020-11-01 19:01:36 -0800

    Solution for - Find K Closest Elements

---
+++ Binary_Search/FindKClosestElements.py
@@ -0,0 +1,31 @@
+# Find K Closest Elements - https://leetcode.com/problems/find-k-closest-elements/
+'''Given a sorted array arr, two integers k and x, find the k closest elements to x in the array. 
+The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]'''
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
+        if arr[0] > x:
+            return arr[:k]
+        
+        if arr[len(arr)-1] < x:
+            return arr[-k:]
+        
+        low = 0
+        high = len(arr)-k
+        while low < high:
+            mid = low + (high-low) // 2
+            if x - arr[mid] > arr[mid+k] - x:
+                low = mid + 1
+            else:
+                high = mid
+        return arr[low:low+k]
