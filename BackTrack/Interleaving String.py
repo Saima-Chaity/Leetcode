7b416430d5ff7cf2d5942b4caba835e64a7cac08
Saima Chaity <saima.s.chaity@gmail.com>
2020-11-20 17:24:51 -0800

    Solution for - Interleaving String

---
+++ BackTrack/Interleaving String.py
@@ -0,0 +1,48 @@
+# Interleaving String - https://leetcode.com/problems/interleaving-string/
+'''Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+
+An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+
+Example 1:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+'''
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+
+        def dfs(s1_index, s2_index, s3_index, s1, s2, s3):
+            if s3_index == len(s3):
+                if s1_index == len(s1) and s2_index == len(s2):
+                    return True
+                else:
+                    return False
+
+            if s1_index == len(s1) and s2_index == len(s2) and s3_index != len(s3):
+                return False
+
+            if (s1_index, s2_index) in cache:
+                return True if cache[s1_index, s2_index] == 1 else False
+
+            result1 = False
+            result2 = False
+            if s1_index < len(s1):
+                if s1[s1_index] == s3[s3_index]:
+                    result1 = dfs(s1_index + 1, s2_index, s3_index + 1, s1, s2, s3)
+
+            if s2_index < len(s2):
+                if s2[s2_index] == s3[s3_index]:
+                    result2 = dfs(s1_index, s2_index + 1, s3_index + 1, s1, s2, s3)
+
+            cache[s1_index, s2_index] = 1 if (result1 or result2) else 0
+            return cache[s1_index, s2_index]
+
+        cache = {}
+        return dfs(0, 0, 0, s1, s2, s3)
