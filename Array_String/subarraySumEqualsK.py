1a00b3de172a0463b148f2f3e14b99abdbde832c
Saima Chaity <saima.s.chaity@gmail.com>
2020-12-16 17:03:15 -0800

    Solution for -  Subarray Sum Equals K

--- Array_String/subarraySumEqualsK.py
+++ Array_String/subarraySumEqualsK.py
@@ -15,18 +15,31 @@ The range of numbers in the array is [-1000, 1000] and the range of the integer 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-        # Single scan. Given the current sum and the k, we check if (sum-k) existsed before at an earlier stage
-        # (at a smaller window size). Keep expanding the sum while checking whether we have seen (sum - k) before
+        # why we are using total - k?
+        # array :: 3 4 7 -2 2 1 4 2
+        # presum :: 3 7 14 12 14 15 19 21
+        # index :: 0 1 2 3 4 5 6 7
+        # k = 7
+        # Map should look like ::
+        # (0,1) , (3,1), (7,1), (14,2) , (12,1) , (15,1) , (19,1) , (21,1)
 
+        # Consider 21(presum) now what we do is sum-k that is 21-7 = 14 . Now we will check our map if we go by just count++ logic we will just increment 
+        # the count once and here is where we go wrong.
+
+        # When we search for 14 in presum array we find it on 2 and 4 index. The logic here is that 14 + 7 = 21 so the array between indexes
+        # -> 3 to 7 (-2 2 1 4 2)
+        # -> 5 to 7 both have sum 7 ( 1 4 2)
+        # The sum is still 7 in this case because there are negative numbers that balance up for. So if we consider count++ we will have one count 
+        # less as we will consider only array 5 to 7 but now we know that 14 sum occured earlier too so even that needs to be added up so map.get(sum-k).
+        
         mapping = {}
         mapping[0] = 1
         total = 0
         count = 0
-
         for num in nums:
             total += num
             if total - k in mapping:
-                count += mapping[total - k]
+                count += mapping[total-k]
             if total in mapping:
                 mapping[total] += 1
             else:
