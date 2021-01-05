09fa861774bdc991a64fc1ccc19284828f425e48
Saima Chaity <saima.s.chaity@gmail.com>
2021-01-05 12:33:50 -0800

    Solution for - Reorder Data in Log Files

---
+++ Array_String/Reorder Data in Log Files.py
@@ -0,0 +1,42 @@
+# Reorder Data in Log Files - https://leetcode.com/problems/reorder-data-in-log-files/
+'''You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that
+each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs
+are ordered lexicographically ignoring identifier, with the identifier used in case of ties.
+The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+Example 1:
+
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+ '''
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+
+        digitLogs = []
+        letterLogs = []
+        for i in range(len(logs)):
+            identifiers, remaining = logs[i].split(" ", 1)
+            if remaining[0].isdigit():
+                digitLogs.append(logs[i])
+            else:
+                letterLogs.append(logs[i])
+
+        def sortedLetterLogs(log):
+            splitedLogs = log.split()
+            identifiers, remaining = splitedLogs[0], splitedLogs[1:]
+            return ' '.join(remaining) + ' ' + identifiers
+
+        letterLogs.sort(key=sortedLetterLogs)
+        return letterLogs + digitLogs
