3037fb29fbaab2c414a9051239bb3b70e53f8b6a
Saima Chaity <saima.s.chaity@gmail.com>
2021-01-20 21:42:29 -0800

    Solution for - Design Tic-Tac-Toe

---
+++ Miscellaneous/Design Tic-Tac-Toe.py
@@ -0,0 +1,134 @@
+# Design Tic-Tac-Toe - https://leetcode.com/problems/design-tic-tac-toe/
+'''Assume the following rules are for the tic-tac-toe game on an n x n board between two players:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves are allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Implement the TicTacToe class:
+
+TicTacToe(int n) Initializes the object the size of the board n.
+int move(int row, int col, int player) Indicates that player with id player plays at the cell (row, col) of the board.
+The move is guaranteed to be a valid move.
+
+Follow up:
+Could you do better than O(n2) per move() operation?
+
+Input
+["TicTacToe", "move", "move", "move", "move", "move", "move", "move"]
+[[3], [0, 0, 1], [0, 2, 2], [2, 2, 1], [1, 1, 2], [2, 0, 1], [1, 0, 2], [2, 1, 1]]
+Output
+[null, 0, 0, 0, 0, 0, 0, 1]
+
+'''
+
+
+class TicTacToe:
+
+    def __init__(self, n: int):
+        """
+        Initialize your data structure here.
+        """
+        self.matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+    def move(self, row: int, col: int, player: int) -> int:
+        """
+        Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins.
+        """
+
+        self.matrix[row][col] = player
+
+        # Check row
+        win = True
+        for i in range(0, len(self.matrix)):
+            if self.matrix[row][i] != player:
+                win = False
+                break
+
+        if win:
+            return player
+
+        # Check col
+        win = True
+        for i in range(0, len(self.matrix)):
+            if self.matrix[i][col] != player:
+                win = False
+                break
+
+        if win:
+            return player
+
+        # Check back diagonal
+        win = True
+        for i in range(0, len(self.matrix)):
+            if self.matrix[i][i] != player:
+                win = False
+                break
+
+        if win:
+            return player
+
+        # check forward diagonal
+        win = True
+        for i in range(0, len(self.matrix)):
+            if self.matrix[i][len(self.matrix) - i - 1] != player:
+                win = False
+                break
+
+        if win:
+            return player
+
+        return 0;
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
+
+
+# Time complexity 0(1)
+
+class TicTacToe:
+
+    def __init__(self, n: int):
+        """
+        Initialize your data structure here.
+        """
+        self.n = n
+        self.rows = [0] * n
+        self.cols = [0] * n
+        self.diagonal1 = 0
+        self.diagonal2 = 0
+
+    def move(self, row: int, col: int, player: int) -> int:
+        """
+        Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins.
+        """
+        value = 1 if player == 1 else -1
+        self.rows[row] += value
+        self.cols[col] += value
+        if row == col:
+            self.diagonal1 += value
+        if col == self.n - row - 1:
+            self.diagonal2 += value
+
+        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diagonal1) == self.n or abs(
+                self.diagonal2) == self.n:
+            return player
+        return 0
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
