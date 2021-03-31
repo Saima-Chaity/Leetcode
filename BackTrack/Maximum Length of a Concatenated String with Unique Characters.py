1b518fe2f43e0320a24e2155fe7962c750ead87a
Saima-Chaity <saima.s.chaity@gmail.com>
2021-03-30 22:33:43 -0700

    Solution for - Maximum Length of a Concatenated String with Unique Characters

---
+++ BackTrack/Maximum Length of a Concatenated String with Unique Characters.py
@@ -0,0 +1,31 @@
+'''Maximum Length of a Concatenated String with Unique Characters
+https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+
+Given an array of strings arr. String s is a concatenation of a sub-sequence of arr which
+have unique characters.
+
+Return the maximum possible length of s.
+
+Example 1:
+
+Input: arr = ["un","iq","ue"]
+Output: 4
+Explanation: All possible concatenations are "","un","iq","ue","uniq" and "ique".
+Maximum length is 4.'''
+
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+
+        result = float('-inf')
+
+        def backTrack(path, index):
+            nonlocal result
+            if len(path) == len(set(path)):
+                result = max(result, len(path))
+            else:
+                return
+            for i in range(index, len(arr)):
+                backTrack(path + arr[i], i + 1)
+
+        backTrack("", 0)
+        return result
